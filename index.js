c039090a90c1fd21e7ae0541035b300a9a9a085f
tanvir005 <tanvir3550gmail.com>
2022-05-26 11:37:14 +0600

    update value from purchase page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,36 +18,70 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-    try {
+  try {
 
-        await client.connect();
-        const partsCollection = client.db('auto_parts_manufactures').collection('parts');
+    await client.connect();
+    const partsCollection = client.db('auto_parts_manufactures').collection('parts');
 
-        app.post('/parts', async (req, res)=>{
-            const parts = req.body;
-            const query = {name: parts.name, description: parts.description, availableQuantity: parts.availableQuantity, minOrderQuantity: parts.minOrderQuantity, unitPrice: parts.unitPrice, img: parts.img};
-            const exist = await partsCollection.findOne(query);
-            if(exist){
-                return res.send({ success: false, parts: exist })
-            }
-            const result = await partsCollection.insertOne(parts);
-            return res.send({ success: true, result });
-        });
+    //putting parts in the db
+    app.post('/parts', async (req, res) => {
+      const parts = req.body;
+      const query = { name: parts.name, description: parts.description, availableQuantity: parts.availableQuantity, minOrderQuantity: parts.minOrderQuantity, unitPrice: parts.unitPrice, img: parts.img };
+      const exist = await partsCollection.findOne(query);
+      if (exist) {
+        return res.send({ success: false, parts: exist })
+      }
+      const result = await partsCollection.insertOne(parts);
+      return res.send({ success: true, result });
+    });
 
-        app.get('/parts',  async (req, res) => {
-            const query = {};
-            const cursor = partsCollection.find(query);
-            const parts = await cursor.toArray();
+    //get all parts to show
+    app.get('/parts', async (req, res) => {
+      const query = {};
+      const cursor = partsCollection.find(query);
+      const parts = await cursor.toArray();
+      res.send(parts);
+    });
+
+    //geeting single part for purchase
+    app.get('/parts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const part = await partsCollection.findOne(query);
+      res.send(part);
+    });
+
+
+    //updating the quantity after purchase
+    app.put('/parts/:id', async (req, res) => {
+      const id = req.params.id;
+      const requestedQuantity = req.body.quantity;
+
+      const filter = { _id: ObjectId(id) };
+      const part = await partsCollection.findOne(filter);
+      const quantity = part.availableQuantity;
+      const newQuantity = quantity - requestedQuantity;
+
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set:
+        {
+          availableQuantity: newQuantity
+        },
+      };
+      const result = await partsCollection.updateOne(filter, updatedDoc, options);
       
-            res.send(parts);
-          });
+      res.send( result);
+    });
 
 
 
-    }
-    finally {
 
-    }
+
+  }
+  finally {
+
+  }
 
 }
 
