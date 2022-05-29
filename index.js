1f63654c729a22492ae53dab59d14f05fae945ea
tanvir005 <tanvir3550gmail.com>
2022-05-29 07:34:37 -1000

    My order delete

--- index.js
+++ index.js
@@ -213,6 +213,15 @@ async function run() {
       res.send(result);
     })
 
+      //delete order
+      app.delete('/order/:id', verifyJWT,  async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const result = await orderCollection.deleteOne(filter);
+        res.send(result);
+      })
+  
+
     
 
     //getting all orders of a user 
